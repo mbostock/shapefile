3c4dfed6996cfe19c4d6f2ee4181845c63a83ff0
Mike Bostock <mbostock@gmail.com>
2013-05-30 09:45:29 -0700

    Minor optimization.

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ function ringContains(ring, point) {
   var x = point[0],
       y = point[1],
       contains = false;
-  for (var i = 0, j = ring.length - 1; i < ring.length; j = i++) {
+  for (var i = 0, n = ring.length, j = n - 1; i < n; j = i++) {
     var pi = ring[i], xi = pi[0], yi = pi[1],
         pj = ring[j], xj = pj[0], yj = pj[1];
     if (((yi > y) ^ (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)) contains = !contains;

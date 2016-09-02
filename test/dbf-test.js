e6c698c2c7f186e0adf4ee09a4fd91e3d4bba036
Mike Bostock <mbostock@gmail.com>
2016-09-02 16:41:51 -0700

    Get tests working!

--- test/dbf-test.js
+++ test/dbf-test.js
@@ -1,164 +1,155 @@
 process.env.TZ = "America/Los_Angeles";
 
-var vows = require("vows"),
-    assert = require("assert");
+var tape = require("tape"),
+    dbf = require("../dbf");
 
-var dbf = require("../dbf");
+function readHeader(path, options) {
+  return dbf.open(path, options)
+    .then((file) => file.header()
+      .catch((error) => file.close().then(() => { throw error; }))
+      .then((header) => file.close().then(() => header)));
+}
 
-var suite = vows.describe("dbf");
+function readRecords(path, options) {
+  return dbf.open(path, options)
+    .then((file) => file.header()
+      .then((header) => {
+        var records = [];
+        return (function next() {
+          return file.record().then((record) => record ? (records.push(record), next()) : records);
+        })();
+      })
+      .catch((error) => file.close().then(() => { throw error; }))
+      .then((records) => file.close().then(() => records)));
+}
 
-suite.addBatch({
-  "The header of a simple dBASE file": {
-    topic: readHeader("./test/boolean-property.dbf"),
-    "has the expected values": function(header) {
-      assert.deepEqual(header, {
-        count: 9,
-        date: new Date(Date.UTC(1995, 6, 26, 7)),
-        version: 3,
-        fields: [{name: "foo", type: "L", length: 1}]
-      });
-    }
-  },
-
-  "The header of an empty dBASE file": {
-    topic: readHeader("./test/empty.dbf"),
-    "has the expected values": function(header) {
-      assert.deepEqual(header, {
-        count: 0,
-        date: new Date(Date.UTC(1995, 6, 26, 7)),
-        version: 3,
-        fields: []
-      });
-    }
-  },
-
-  "The header of a dBASE file with ISO-8859-1 property names": {
-    topic: readHeader("./test/latin1-property.dbf"),
-    "has the expected values": function(header) {
-      assert.deepEqual(header, {
-        count: 1,
-        date: new Date(Date.UTC(1995, 6, 26, 7)),
-        version: 3,
-        fields: [{name: "name", type: "C", length: 80}]
-      });
-    }
-  },
-
-  "The records of a dBASE file with ISO-8859-1 property names": {
-    topic: readRecords("./test/latin1-property.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        ["México"]
-      ]);
-    }
-  },
-
-  "The header of a dBASE file with UTF-8 property names": {
-    topic: readHeader("./test/utf8-property.dbf", "utf8"),
-    "has the expected values": function(header) {
-      assert.deepEqual(header, {
-        count: 1,
-        date: new Date(Date.UTC(1995, 6, 26, 7)),
-        version: 3,
-        fields: [{name: "☃", type: "C", length: 80}]
-      });
-    }
-  },
-
-  "The records of a dBASE file with UTF-8 property names": {
-    topic: readRecords("./test/utf8-property.dbf", "utf8"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        ["ηελλο ςορλδ"]
-      ]);
-    }
-  },
-
-  "The records of an empty dBASE file": {
-    topic: readRecords("./test/empty.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, []);
-    }
-  },
-
-  "The records of a simple dBASE file": {
-    topic: readRecords("./test/boolean-property.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        [null],
-        [true],
-        [true],
-        [false],
-        [false],
-        [true],
-        [true],
-        [false],
-        [false]
-      ]);
-    }
-  },
-
-  "The records of a dBASE file with a string property": {
-    topic: readRecords("./test/string-property.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        [null],
-        ["blue"],
-        ["green"]
-      ]);
-    }
-  },
-
-  "The records of a dBASE file with a number property": {
-    topic: readRecords("./test/number-property.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        [null],
-        [42],
-        [-4]
-      ]);
-    }
-  },
-
-  "The records of a dBASE file with a date property": {
-    topic: readRecords("./test/date-property.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        [new Date(2013, 0, 2)],
-        [new Date(2013, 1, 2)],
-        [new Date(2013, 0, 3)]
-      ]);
-    }
-  },
-
-  "The records of a dBASE file with multiple properties": {
-    topic: readRecords("./test/mixed-properties.dbf"),
-    "have the expected values": function(records) {
-      assert.deepEqual(records, [
-        [null, null],
-        [42, null],
-        [null, "blue"]
-      ]);
-    }
-  }
+tape("The header of a simple dBASE file", function(test) {
+  readHeader("./test/boolean-property.dbf")
+    .then((header) => test.deepEqual(header, {
+      version: 3,
+      length: 9,
+      date: new Date(Date.UTC(1995, 6, 26, 7)),
+      fields: [{name: "foo", type: "L", length: 1}]
+    }))
+    .then(() => test.end());
 });
 
-function readHeader(path, encoding) {
-  return function() {
-    var callback = this.callback;
-    dbf.read(path, encoding, function(error, header, records) {
-      callback(error, header);
-    });
-  };
-}
+tape("The header of an empty dBASE file", function(test) {
+  readHeader("./test/empty.dbf")
+    .then((header) => test.deepEqual(header, {
+      version: 3,
+      length: 0,
+      date: new Date(Date.UTC(1995, 6, 26, 7)),
+      fields: []
+    }))
+    .then(() => test.end());
+});
 
-function readRecords(path, encoding) {
-  return function() {
-    var callback = this.callback;
-    dbf.read(path, encoding, function(error, header, records) {
-      callback(error, records);
-    });
-  };
-}
+tape("The header of a dBASE file with ISO-8859-1 property names", function(test) {
+  readHeader("./test/latin1-property.dbf")
+    .then((header) => test.deepEqual(header, {
+      length: 1,
+      date: new Date(Date.UTC(1995, 6, 26, 7)),
+      version: 3,
+      fields: [{name: "name", type: "C", length: 80}]
+    }))
+    .then(() => test.end());
+});
 
-suite.export(module);
+tape("The records of a dBASE file with ISO-8859-1 property names", function(test) {
+  dbf.open("./test/latin1-property.dbf")
+    .then((file) => file.header()
+      .then((header) => {
+        var records = [];
+        return (function next() {
+          return file.record().then((record) => record ? (records.push(record), next()) : records);
+        })();
+      })
+      .then((records) => test.deepEqual(records, [
+        ["México"]
+      ]))
+      .then(() => file.close()))
+    .then(() => test.end())
+    .catch((error) => file.close().then(() => test.end(error)));
+});
+
+tape("The header of a dBASE file with UTF-8 property names", function(test) {
+  readHeader("./test/utf8-property.dbf", {encoding: "utf8"})
+    .then((header) => test.deepEqual(header, {
+      length: 1,
+      date: new Date(Date.UTC(1995, 6, 26, 7)),
+      version: 3,
+      fields: [{name: "☃", type: "C", length: 80}]
+    }))
+    .then(() => test.end());
+});
+
+tape("The records of a dBASE file with UTF-8 property names", function(test) {
+  readRecords("./test/utf8-property.dbf", {encoding: "utf8"})
+    .then((records) => test.deepEqual(records, [
+      ["ηελλο ςορλδ"]
+    ]))
+    .then(() => test.end());
+});
+
+tape("The records of a dBASE file with UTF-8 property names", function(test) {
+  readRecords("./test/empty.dbf")
+    .then((records) => test.deepEqual(records, []))
+    .then(() => test.end());
+});
+
+tape("The records of a simple dBASE file", function(test) {
+  readRecords("./test/boolean-property.dbf")
+    .then((records) => test.deepEqual(records, [
+      [null],
+      [true],
+      [true],
+      [false],
+      [false],
+      [true],
+      [true],
+      [false],
+      [false]
+    ]))
+    .then(() => test.end());
+});
+
+tape("The records of a dBASE file with a string property", function(test) {
+  readRecords("./test/string-property.dbf")
+    .then((records) => test.deepEqual(records, [
+      [null],
+      ["blue"],
+      ["green"]
+    ]))
+    .then(() => test.end());
+});
+
+tape("The records of a dBASE file with a number property", function(test) {
+  readRecords("./test/number-property.dbf")
+    .then((records) => test.deepEqual(records, [
+      [null],
+      [42],
+      [-4]
+    ]))
+    .then(() => test.end());
+});
+
+tape("The records of a dBASE file with a date property", function(test) {
+  readRecords("./test/date-property.dbf")
+    .then((records) => test.deepEqual(records, [
+      [new Date(2013, 0, 2)],
+      [new Date(2013, 1, 2)],
+      [new Date(2013, 0, 3)]
+    ]))
+    .then(() => test.end());
+});
+
+tape("The records of a dBASE file with a multiple properties", function(test) {
+  readRecords("./test/mixed-properties.dbf")
+    .then((records) => test.deepEqual(records, [
+      [null, null],
+      [42, null],
+      [null, "blue"]
+    ]))
+    .then(() => test.end());
+});

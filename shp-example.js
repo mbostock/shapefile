416e75bf48e2e29a2165941e958d455bf15df50c
Mike Bostock <mbostock@gmail.com>
2016-09-01 20:49:24 -0700

    Yay, fat arrow.

--- shp-example.js
+++ shp-example.js
@@ -1,16 +1,13 @@
 var shp = require("./shp");
 
 shp.open("test/null.shp")
-  .then(function(points) {
-    return points.header()
-      .then(function(header) { console.log("header", header); })
-      .then(function next() {
-        return points.record().then(function(record) {
-          if (!record) return;
-          console.log("record", record);
-          return next();
-        });
-      })
-      .then(function() { return points.close(); });
-  })
-  .catch(function(error) { console.error(error.stack); });
+  .then((points) => points.header()
+    .then((header) => console.log("header", header))
+    .then(function next() {
+      return points.record().then((record) => record
+          ? (console.log("record", record), next())
+          : console.log("end"));
+    })
+    .catch((error) => points.close().then(() => { throw error; }))
+    .then(() => points.close()))
+  .catch((error) => console.error(error.stack));

4a807563ba79b49f2d0c88c50de9814a76df261f
Mike Bostock <mbostock@gmail.com>
2016-09-03 09:14:05 -0700

    Slightly simpler.

--- shp/record.js
+++ shp/record.js
@@ -1,9 +1,7 @@
 var readNull = require("./null");
 
 module.exports = function() {
-  return this._file.read(8).then((buffer) => buffer.length
-      ? this._file.read(buffer.readInt32BE(4) * 2).then((buffer) => buffer.readInt32LE(0)
-          ? this._type(buffer)
-          : readNull())
-      : null);
+  return this._file.skip(4).readInt32BE()
+    .then((length) => length == null ? null : this._file.read(length * 2)
+      .then((buffer) => buffer.readInt32LE(0) ? this._type(buffer) : readNull()));
 };

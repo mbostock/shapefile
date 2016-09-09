498334c241c2b8e76c3601ec55d13fbb99ff4507
Mike Bostock <mbostock@gmail.com>
2016-09-09 09:13:59 -0700

    Remove unused per-record bounding box.

--- shp/multipoint.js
+++ shp/multipoint.js
@@ -1,5 +1,5 @@
 export default function(record) {
   var i = 40, j, n = record.getInt32(36, true), points = new Array(n);
   for (j = 0; j < n; ++j, i += 16) points[j] = [record.getFloat64(i, true), record.getFloat64(i + 8, true)];
-  return {shapeType: 8, box: [record.getFloat64(4, true), record.getFloat64(12, true), record.getFloat64(20, true), record.getFloat64(28, true)], points: points};
+  return {shapeType: 8, points: points};
 };
--- shp/poly.js
+++ shp/poly.js
@@ -3,6 +3,6 @@ export default function(shapeType) {
     var i = 44, j, n = record.getInt32(36, true), m = record.getInt32(40, true), parts = new Array(n), points = new Array(m);
     for (j = 0; j < n; ++j, i += 4) parts[j] = record.getInt32(i, true);
     for (j = 0; j < m; ++j, i += 16) points[j] = [record.getFloat64(i, true), record.getFloat64(i + 8, true)];
-    return {shapeType: shapeType, box: [record.getFloat64(4, true), record.getFloat64(12, true), record.getFloat64(20, true), record.getFloat64(28, true)], parts: parts, points: points};
+    return {shapeType: shapeType, parts: parts, points: points};
   };
 };

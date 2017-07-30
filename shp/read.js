74dfc83907eb185d28c154fc237f273aa0d3b578
Mike Bostock <mbostock@gmail.com>
2017-07-30 15:27:41 -0700

    Fix #36 for ne_10m_railroads.shp.
    Advance by four bytes instead of eight bytes.
    And instead of crashing on empty records, treat them as null objects.

--- shp/read.js
+++ shp/read.js
@@ -9,19 +9,19 @@ export default function() {
     var header = view(array), type = header.getInt32(8, true);
 
     // If the record starts with an invalid shape type (see #36), scan ahead in
-    // eight-byte increments to find the next valid object. This object is
+    // four-byte increments to find the next valid object. This object is
     // identified by a header containing the expected index and a record
     // starting with an valid shape type.
     function scan() {
-      return that._source.slice(8).then(function(chunk) {
+      return that._source.slice(4).then(function(chunk) {
         if (chunk == null) return {done: true, value: undefined};
-        header = view(array = concat(array.slice(8), chunk)), type = header.getInt32(8, true);
+        header = view(array = concat(array.slice(4), chunk)), type = header.getInt32(8, true);
         return header.getInt32(0, false) !== that._index || (type && type !== that._type) ? scan() : read();
       });
     }
 
     function read() {
-      return that._source.slice(header.getInt32(4, false) * 2 - 4).then(function(chunk) {
+      return that._source.slice(Math.max(0, header.getInt32(4, false) * 2 - 4)).then(function(chunk) {
         return {done: false, value: type ? that._parse(view(concat(array.slice(8), chunk))) : null};
       });
     }

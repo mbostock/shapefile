ef5db94f1d8fec9c157bace71404e8d66e613f72
Mike Bostock <mbostock@gmail.com>
2017-07-30 21:06:37 -0700

    Fix trailing null records.
    If the content length is zero, it should be skipped rather than treated as a
    null geometry.

--- shp/read.js
+++ shp/read.js
@@ -6,26 +6,28 @@ export default function() {
   ++that._index;
   return that._source.slice(12).then(function(array) {
     if (array == null) return {done: true, value: undefined};
-    var header = view(array), type = header.getInt32(8, true);
+    var header = view(array);
 
     // If the record starts with an invalid shape type (see #36), scan ahead in
-    // four-byte increments to find the next valid object. This object is
-    // identified by a header containing the expected index and a record
-    // starting with an valid shape type.
-    function scan() {
+    // four-byte increments to find the next valid record, identified by the
+    // expected index, a non-empty content length and a valid shape type.
+    function skip() {
       return that._source.slice(4).then(function(chunk) {
         if (chunk == null) return {done: true, value: undefined};
-        header = view(array = concat(array.slice(4), chunk)), type = header.getInt32(8, true);
-        return header.getInt32(0, false) !== that._index || (type && type !== that._type) ? scan() : read();
+        header = view(array = concat(array.slice(4), chunk));
+        return header.getInt32(0, false) !== that._index ? skip() : read();
       });
     }
 
+    // All records should have at least four bytes (for the record shape type),
+    // so an invalid content length indicates corruption.
     function read() {
-      return that._source.slice(Math.max(0, header.getInt32(4, false) * 2 - 4)).then(function(chunk) {
+      var length = header.getInt32(4, false) * 2 - 4, type = header.getInt32(8, true);
+      return length < 0 || (type && type !== that._type) ? skip() : that._source.slice(length).then(function(chunk) {
         return {done: false, value: type ? that._parse(view(concat(array.slice(8), chunk))) : null};
       });
     }
 
-    return type && type !== that._type ? scan() : read();
+    return read();
   });
 }

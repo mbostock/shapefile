7acfde2a47c3ddf379640994bda9ebd82af43de7
Mike Bostock <mbostock@gmail.com>
2017-09-02 15:14:44 -0700

    Update dependencies.
    Also replaces the prepublish script with prepublishOnly per npm, and replaces
    the browser field in package.json with unpkg and jsdelivr; see d3/d3#3138.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
 import node from "rollup-plugin-node-resolve";
 
 export default {
-  plugins: [node()]
-}
+  input: "index",
+  plugins: [node()],
+  output: {
+    file: "dist/shapefile.js",
+    format: "umd",
+    name: "shapefile"
+  }
+};
